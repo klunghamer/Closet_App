1877624c0ca7401cb2765836b5a06f6163a36278
Kelly Lunghamer <klung@umich.edu>
2016-10-03 15:39:54 -0400

    Created initial server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+//Express
+var express = require('express');
+var app = express();
+
+//Debugging
+var logger = require('morgan');
+app.use(logger('dev'));
+
+//Handlebars
+var hbs = require('hbs');
+app.set('view engine', 'hbs');
+require('handlebar-form-helpers').register(hbs.handlebars);
+
+//Mongoose
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/closet');
+var db = mongoose.connection;
+db.on('error', function(err) {
+  console.log(err);
+})
+db.once('open', function() {
+  console.log('database connected!');
+})
+
+app.listen(4000);
