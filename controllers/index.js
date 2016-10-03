81c010e3afc51da69157a6d3f9b82ce2b9c51914
Kelly Lunghamer <klung@umich.edu>
2016-10-03 17:44:37 -0400

    Creating new user is functioning

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,31 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
+//Landing page
 router.get('/', function (req,res) {
   res.render('home', {
     title: 'Home page',
-    user: 'user'
   })
+});
+
+//Sign up user and go back to landing page
+router.post('/signup', function (req,res) {
+  User.register(
+    new User({
+      username: req.body.username,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(user);
+      }
+      res.redirect('/')
+    }
+  )
 });
 
 
